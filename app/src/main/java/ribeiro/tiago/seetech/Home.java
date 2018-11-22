a1998d0969b35a9401e6799de5f03e48f444fda7
Unknown <tiagofr98@gmail.com>
2018-11-22 02:52:37 -0200

    Tela inicial
    Identidade visual - ok

---
+++ app/src/main/java/ribeiro/tiago/seetech/Home.java
@@ -0,0 +1,22 @@
+package ribeiro.tiago.seetech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void BtnEntrar_OnClick(View view){
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+    }
+}
